3887e18c42445fddd7137c9fd4395d35748e1a16
Sarunas Navickas <zaibacu@gmail.com>
2021-01-01 12:14:50 +0200

    More stuff

--- utils.py
+++ utils.py
@@ -32,11 +32,63 @@ def bruteforce_generator(chars, min_length=3, max_length=20):
         n += 1
 
 
-def find_passw(passw, chars=string.printable):
+def load_passwordlist(path):
+    with open(path, "r") as f:
+        raw = f.readlines()
+
+    return [l.strip() for l in raw if not l.startswith("#") and l.strip() != ""]
+
+
+def find_passw1(passw, chars=string.printable):
     t = Timer("Finding n={}".format(len(passw)))
+    found = False
     for i in bruteforce_generator(chars, min_length=3, max_length=len(passw)):
         if i == passw:
             print("Found: {}".format(i))
+            found = True
             break
 
     t.stop()
+    if not found:
+        print("NOT FOUND")
+
+
+def find_passw2(passw):
+    t = Timer("Finding n={}".format(len(passw)))
+    lst = list(load_passwordlist("password-list.txt"))
+    found = False
+    for i in lst:
+        if i == passw:
+            print("Found: {}".format(i))
+            found = True
+            break
+    t.stop()
+    if not found:
+        print("NOT FOUND")
+
+
+def mutate_passw(lst):
+    popular_numbers = list(range(1, 99)) + list(range(1900, 2021))
+    for l in lst:
+        yield l
+        yield l.lower()
+        yield l[0].upper() + l[1:].lower()
+        for n in popular_numbers:
+            yield l + str(n)
+
+
+def find_passw3(passw):
+    t = Timer("Finding n={}".format(len(passw)))
+    lst = list(load_passwordlist("password-list.txt"))
+    found = False
+    for i in mutate_passw(lst):
+        if i == passw:
+            print("Found: {}".format(i))
+            found = True
+            break
+
+    t.stop()
+    if not found:
+        print("NOT FOUND")
+
+
